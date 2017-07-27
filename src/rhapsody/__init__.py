d46e8c939655bd33ce1873481076e57c5c9286ce
Verth-Elone <verthelone@gmail.com>
2017-07-27 18:39:11 +0200

    Changed to package

---
+++ src/rhapsody/__init__.py
@@ -0,0 +1,6 @@
+# Copyright (c) Peter Majko.
+# See LICENSE for details.
+
+"""
+Rhapsody: Rapid Heterogeneous Applications Development Framework.
+"""
---
+++ src/rhapsody/__main__.py
@@ -0,0 +1,5 @@
+# Copyright (c) Peter Majko.
+# See LICENSE for details.
+
+if __name__ == '__main__':
+    pass
