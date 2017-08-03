711e1db72fa4d8831ceb9d861f6357a7ebf59e31
Verth-Elone <verthelone@gmail.com>
2017-08-03 14:46:03 +0200

    Redefined logging to log, added functions

---
+++ src/rhapsody/io/log.py
@@ -0,0 +1,53 @@
+# Copyright (c) Peter Majko.
+
+"""
+Rhapsody's logger module
+"""
+
+from rhapsody.core.multiprocess import PipeHandler
+import logging
+
+
+def get_default_logger(name=None):
+    logger = logging.getLogger(name)
+    if not logger.hasHandlers():
+        formatter = logging.Formatter(fmt='%(asctime)s|%(levelname)s|%(name)s: %(message)s',
+                                      datefmt='%d.%m.%Y %T')
+        handler = logging.StreamHandler()
+        handler.setFormatter(formatter)
+        handler.setLevel(logging.DEBUG)
+        logger.addHandler(handler)
+        logger.setLevel(logging.DEBUG)
+    return logger
+
+
+def get_child_logger(parent_logger: logging.Logger, child_logger_name: str):
+    logger = logging.getLogger('{pln}.{cln}'.format(pln=parent_logger.name, cln=child_logger_name))
+    return logger
+
+
+class LoggingPipeHandler(logging.Handler):
+    """
+
+    """
+    def __init__(self, pipe_handler: PipeHandler, level=logging.NOTSET):
+        super().__init__(level)
+        self.pipe_handler = pipe_handler
+
+    def emit(self, record):
+        log_entry = self.format(record)
+        self.pipe_handler.send(log_entry)
+
+
+class PipeStream:
+    """
+    Simple stream which streams to the pipe connection
+    """
+    def __init__(self, pipe_handler: PipeHandler):
+        self.pipe_handler = pipe_handler
+
+    def write(self, msg):
+        self.pipe_handler.send(msg)
+
+    def flush(self):
+        pass
