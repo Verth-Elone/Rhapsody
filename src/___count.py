fe9d655bbae007e5a7e8cb68e60b8abd646cb70e
Verth-Elone <verthelone@gmail.com>
2017-08-15 15:03:50 +0200

    changed count to include characters

--- src/___count.py
+++ src/___count.py
@@ -1,28 +1,40 @@
 from os import listdir, getcwd, walk
 from os.path import isfile, join
 
-
 def countLinesInPath(path, directory):
-    count = 0
+    line_count = 0
+    char_count = 0
     file = open(join(directory, path), encoding='utf8')
     for line in file:
-        count += 1
-    return count
-
+        line_count += 1
+        char_count += len(line)
+    return line_count, char_count
 
 def countLines(paths, directory):
-    count = 0
+    line_count = 0
+    char_count = 0
     for path in paths:
         if path[len(path)-3:len(path)] == '.py':
-            count = count + countLinesInPath(path, directory)
-    return count
+            lc, cc = countLinesInPath(path, directory)
+            line_count += lc
+            char_count += cc
+    return line_count, char_count
 
 
 def countIn(directory):
-    count = 0
+    line_count = 0
+    char_count = 0
     for root, dirs, files in walk(directory):
-        count += countLines(files, root)
-    return count
+        lc, cc = countLines(files, root)
+        line_count += lc
+        char_count += cc
+    return line_count, char_count
 
 if __name__ == '__main__':
-    print('Total project lines length: {}'.format(countIn(getcwd()) - countLinesInPath(__file__, '')))
+    line_count_total, char_count_total = countIn(getcwd())
+    line_count_this, char_count_this = countLinesInPath(__file__, '')
+    line_count_cleaned = line_count_total - line_count_this
+    char_count_cleaned = char_count_total - char_count_this
+
+    print('Total project lines length: {}\nTotal project char length: {}'.format(line_count_cleaned,
+                                                                                 char_count_cleaned))
